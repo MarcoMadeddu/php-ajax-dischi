af1d6f217eaa9db207342a5c36ba36c85d0d6092
MarcoMadeddu <marcomade99@gmail.com>
2020-05-20 16:12:21 +0200

    set

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotify with php</title>
+     <!-- M A I N  C S S -->
+     <link rel="stylesheet" href="dist/css/main.css">
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+     <!-- J A V A  S C R I P T -->
+     <script src="dist/js/main.js"></script>
+</body>
+</html>
