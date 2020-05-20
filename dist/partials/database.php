0806f70a8fe2ccbe2cf91a7637dce5f81fd21c66
MarcoMadeddu <marcomade99@gmail.com>
2020-05-20 17:27:30 +0200

    milestone 1 completed

---
+++ dist/partials/database.php
@@ -0,0 +1,66 @@
+<?php
+$database = [
+    [
+        'poster' => 'dist/img/geolier.jpeg',
+        'title' => 'New York',
+        'author' => 'Geolier',
+        'year' => '2019',
+    ],
+
+    [
+        'poster' => "dist/img/shiva-pyrex.jpeg",
+        'title' => 'Niente',
+        'author' => 'Giaime ft Pyrex',
+        'year' => '2020',
+    ],
+
+    [
+        'poster' => "dist/img/lazza.jpeg",
+        'title' => 'Frio',
+        'author' => 'Lazza',
+        'year' => '2019',
+    ],
+
+    [
+        'poster' => "dist/img/marracash.jpeg",
+        'title' => 'Sport',
+        'author' => 'Marracash & Luchè',
+        'year' => '2019',
+    ],
+
+    [
+        'poster' => "dist/img/shiva.png",
+        'title' => 'Calmo',
+        'author' => 'Shiva & Tha Supreme',
+        'year' => '2019',
+    ],
+
+    [
+        'poster' => "dist/img/giaime.jpeg",
+        'title' => 'Parola',
+        'author' => 'Giaime',
+        'year' => '2019',
+    ],
+
+    [
+        'poster' => "dist/img/gue.jpeg",
+        'title' => 'Daily mix 1',
+        'author' => 'Gue e altri',
+        'year' => '2020',
+    ],
+
+    [
+        'poster' =>"dist/img/tha-supreme.jpeg",
+        'title' => 'Daily mix 2',
+        'author' => 'Tha Supreme e altri',
+        'year' => '2020',
+    ],
+
+    [
+        'poster' => "dist/img/ava.jpeg",
+        'title' => 'Holly e Benji',
+        'author' => 'Shiva & CapoPlaza',
+        'year' => '2019',
+    ],
+]
+?>
--- index.php
+++ index.php
@@ -8,7 +8,31 @@
      <link rel="stylesheet" href="dist/css/main.css">
 </head>
 <body>
-    
+    <div class="app">
+        <header>
+            <div class="header-element">
+                <img src="dist/img/logo-small.svg" alt="">
+            </div>
+        </header>
+
+        <main>
+            <div class="main-element">
+                <?php include_once __DIR__ . '/dist/partials/database.php' ?>
+
+                <?php foreach($database as $data) {?>
+                    <div class="card">
+                    <img src="<?php echo $data['poster']; ?>" alt="we">
+                    <h2><?php echo$data['title']; ?></h2>
+                    <h3><?php echo$data['author']; ?></h3>
+                    <h3><?php echo$data['year']; ?></h3>
+                    </div>
+                    
+
+                <?php } ?>
+            </div>
+        </main>
+
+    </div>
 
 
 
